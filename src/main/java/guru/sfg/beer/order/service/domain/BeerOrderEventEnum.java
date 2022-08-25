c1a8f729aec45d7d1b6b1c297527fa23e05b0b0d
BASSET, Thomas <thomas.basset@cgi.com>
2022-08-25 11:14:44 +0200

    Setup Allocation State Machine States. closes #11

--- src/main/java/guru/sfg/beer/order/service/domain/BeerOrderEventEnum.java
+++ src/main/java/guru/sfg/beer/order/service/domain/BeerOrderEventEnum.java
@@ -1,6 +1,6 @@
 package guru.sfg.beer.order.service.domain;
 
-public enum BeerOrderEvent {
+public enum BeerOrderEventEnum {
     VALIDATE_ORDER, VALIDATION_PASSED, VALIDATION_FAILED,
     ALLOCATION_SUCCESS, ALLOCATION_NO_INVENTORY, ALLOCATION_FAILED,
     BEER_ORDER_PICKED_UP, CANCEL_ORDER
---
+++ src/main/java/guru/sfg/beer/order/service/sm/BeerOrderStateMachineConfig.java
@@ -0,0 +1,31 @@
+package guru.sfg.beer.order.service.sm;
+
+import guru.sfg.beer.order.service.domain.BeerOrderEventEnum;
+import guru.sfg.beer.order.service.domain.BeerOrderStatusEnum;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.config.EnableStateMachineFactory;
+import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+
+import java.util.EnumSet;
+
+@Slf4j
+@EnableStateMachineFactory
+@RequiredArgsConstructor
+@Configuration
+public class BeerOrderStateMachineConfig extends StateMachineConfigurerAdapter<BeerOrderStatusEnum, BeerOrderEventEnum> {
+
+    @Override
+    public void configure(StateMachineStateConfigurer<BeerOrderStatusEnum, BeerOrderEventEnum> states) throws Exception {
+        states.withStates()
+                .initial(BeerOrderStatusEnum.NEW)
+                .states(EnumSet.allOf(BeerOrderStatusEnum.class))
+                .end(BeerOrderStatusEnum.ALLOCATION_EXCEPTION)
+                .end(BeerOrderStatusEnum.DELIVERY_EXCEPTION)
+                .end(BeerOrderStatusEnum.VALIDATION_EXCEPTION)
+                .end(BeerOrderStatusEnum.CANCELED)
+                .end(BeerOrderStatusEnum.DELIVERED);
+    }
+}
