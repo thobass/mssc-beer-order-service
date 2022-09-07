e50ec8ab1168c172244a4cae8d80731b62306690
BASSET, Thomas <thomas.basset@cgi.com>
2022-09-07 20:53:59 +0200

    Configure Spring Cloud Config Discovery Client. closes #38

--- src/test/java/guru/sfg/beer/order/service/testcomponents/BeerOrderValidationListener.java
+++ src/test/java/guru/sfg/beer/order/service/testcomponents/BeerOrderValidationListener.java
@@ -1,37 +1,45 @@
 package guru.sfg.beer.order.service.testcomponents;
 
+import guru.sfg.beer.order.service.config.JmsConfig;
+import guru.sfg.brewery.model.events.ValidateOrderRequest;
+import guru.sfg.brewery.model.events.ValidateOrderResult;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
+
+import java.util.Objects;
 
 @Slf4j
 @RequiredArgsConstructor
 @Component
 public class BeerOrderValidationListener {
-//    private final JmsTemplate jmsTemplate;
-//
-//    @JmsListener(destination = JmsConfig.VALIDATE_ORDER_QUEUE)
-//    public void list(Message msg) {
-//        boolean isValid = true;
-//        boolean sendResponse = true;
-//
-//        ValidateOrderRequest request = (ValidateOrderRequest) msg.getPayload();
-//
-//        if (Objects.equals(request.getBeerOrder().getCustomerRef(), "fail-validation")) {
-//            isValid = false;
-//        }
-//
-//        //set cancel error
-//        if (Objects.equals(request.getBeerOrder().getCustomerRef(), "dont-validate")) {
-//            sendResponse = false;
-//        }
-//
-//        if (sendResponse) {
-//            jmsTemplate.convertAndSend(JmsConfig.VALIDATE_ORDER_RESPONSE_QUEUE,
-//                    ValidateOrderResult.builder()
-//                            .isValid(isValid)
-//                            .orderId(request.getBeerOrder().getId())
-//                            .build());
-//        }
-//    }
+    private final JmsTemplate jmsTemplate;
+
+    @JmsListener(destination = JmsConfig.VALIDATE_ORDER_QUEUE)
+    public void list(Message msg) {
+        boolean isValid = true;
+        boolean sendResponse = true;
+
+        ValidateOrderRequest request = (ValidateOrderRequest) msg.getPayload();
+
+        if (Objects.equals(request.getBeerOrder().getCustomerRef(), "fail-validation")) {
+            isValid = false;
+        }
+
+        //set cancel error
+        if (Objects.equals(request.getBeerOrder().getCustomerRef(), "dont-validate")) {
+            sendResponse = false;
+        }
+
+        if (sendResponse) {
+            jmsTemplate.convertAndSend(JmsConfig.VALIDATE_ORDER_RESPONSE_QUEUE,
+                    ValidateOrderResult.builder()
+                            .isValid(isValid)
+                            .orderId(request.getBeerOrder().getId())
+                            .build());
+        }
+    }
 }
